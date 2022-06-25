727479e7ecca2c61833d23358278aebd47e8b38f
Kalesanwo <kalesanwoyinka99@gmail.com>
2022-06-25 13:52:48 +0100

    django crud

---
+++ olayinka22/blog/views.py
@@ -0,0 +1,29 @@
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView
+from django.urls import reverse_lazy
+from .models import blog
+from django.urls import path
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = “__all__”
+    success_url = reverse_lazy (“blog: all”)
+
+class PostDetailView(DetailView):
+     model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = “__all__”
+    success_url  = reverse_lazy(“blog:all”)
+
+class PostDeleteView(UpdateView):
+    model = Post
+    fields = “__all__”
+    success_url  = reverse_lazy(“blog:all”)
